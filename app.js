803314381762e91dccba0ef44300010bfc32d962
Alex <akosialexpo@gmail.com>
2017-11-17 02:19:09 +0800

    Add footer and favicon

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.use(
   })
 );
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
